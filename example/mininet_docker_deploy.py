07862fa975bf65b64a7e1608ac93552740281520
DmitryMurashov <dmtr.murashov@gmail.com>
2025-03-08 13:38:35 +0300

    index on dev: 4017dbe [setup] Build |  Updated python version

--- example/mininet_docker_deploy.py
+++ example/mininet_docker_deploy.py
@@ -10,36 +10,47 @@ import mininet.net
 import mininet.node
 import pathlib
 import tired.logging
+import os
 
 
 def main():
+    os.system('cgroupfs-umount')
+    os.system('cgroupfs-mount')
+
     setLogLevel('debug')
     tired.logging.set_level(tired.logging.DEBUG)
 
     # Build the basic network topology
-    net = mininet.net.Mininet(controller=mininet.node.Controller)
-    c1 = net.addController("c0")
+    net = mininet.net.Mininet(controller=mininet.node.Controller, switch=mininet.node.OVSSwitch, waitConnected=True)
+    c1 = net.addController("c1", port=6633)
     s1 = net.addSwitch("s1")
     h1 = net.addHost("h1")
     h2 = net.addHost("h2")
-    lh1s1 = net.addLink(h1, s1)
-    lh2s1 = net.addLink(h2, s1)
-    lc1s1 = net.addLink(c1, s1)
+    lh1s1 = net.addLink(s1, h1)
+    lh2s1 = net.addLink(s1, h2)
 
     # Run the network
     tired.logging.info("starting the network")
+    net.build()
     net.start()
+    c1.start()
+    s1.start([c1])
     # Get network summary
-    howlitbe.mininet.log_network_summary(net)
+    # howlitbe.mininet.log_network_summary(net)
 
     # Deploy docker in the network namespaces of hosts h1, and h2
-    deployscriptpath = pathlib.Path(__file__).resolve().parent / "res" / "docker-deploy-debian.sh"
-    cmd = f'unshare --croup --mount /bin/bash {deployscriptpath}&'
-    h1.sendCmd()
+    deployscriptpath = pathlib.Path(__file__).resolve().parent / "docker-deploy-debian.sh"
+    hosts = [h1, h2]
+    for i in range(2):
+        cmd = f'unshare --mount --pid --fork --ipc --cgroup /bin/bash {deployscriptpath} {i}'
+        hosts[i].sendCmd(cmd)
+    tired.logging.info(f"Deploying Docker on h1")
 
     # Drop into Mininet shell
     tired.logging.info("Dropping into containernet shell")
     CLI(net)
+
+    net.stop()
 
 
 if __name__ == "__main__":
